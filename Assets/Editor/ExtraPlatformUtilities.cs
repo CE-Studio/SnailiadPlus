298c91f647788c636e33137c2e728cd8d3905336
EpsilonTheDerg <jamesclamey@gmail.com>
2023-12-06 22:55:01 -0500

    Improved platform generation and added more variants

--- Assets/Editor/ExtraPlatformUtilities.cs
+++ Assets/Editor/ExtraPlatformUtilities.cs
@@ -9,25 +9,7 @@ public class ExtraPlatformUtilities : Editor
     ExtraPlatform script;
     SpriteRenderer sprite;
 
-    public Sprite plat0;
-    public Sprite plat1;
-    public Sprite plat2;
-    public Sprite plat3;
-    public Sprite plat4;
-    public Sprite plat5;
-    public Sprite plat6;
-    public Sprite plat7;
-    public Sprite plat8;
-    public Sprite plat9;
-    public Sprite plat10;
-    public Sprite plat11;
-    public Sprite plat12;
-    public Sprite plat13;
-    public Sprite plat14;
-    public Sprite plat15;
-    public Sprite plat16;
-    public Sprite plat17;
-    public Sprite plat18;
+    public Sprite[] platSprites;
 
     SerializedProperty sCharStates;
     SerializedProperty sType;
@@ -39,6 +21,8 @@ public class ExtraPlatformUtilities : Editor
 
         sCharStates = serializedObject.FindProperty("appearToChars");
         sType = serializedObject.FindProperty("type");
+
+        platSprites = Resources.LoadAll<Sprite>("Images/Entities/ExtraPlatform");
     }
 
     public override void OnInspectorGUI()
@@ -54,15 +38,11 @@ public class ExtraPlatformUtilities : Editor
         sCharStates.GetArrayElementAtIndex(5).boolValue = EditorGUILayout.Toggle("Leechy", sCharStates.GetArrayElementAtIndex(5).boolValue);
 
         GUILayout.Space(5);
-        sType.intValue = EditorGUILayout.IntField("Platform type", Mathf.Clamp(sType.intValue, 0, 18));
+        sType.intValue = EditorGUILayout.IntField("Platform type", Mathf.Clamp(sType.intValue, 0, platSprites.Length - 1));
 
         serializedObject.ApplyModifiedProperties();
 
-        if (sType.intValue >= 0 && sType.intValue <= 18)
-            sprite.sprite = new Sprite[]
-            {
-                plat0, plat1, plat2, plat3, plat4, plat5, plat6, plat7, plat8, plat9,
-                plat10, plat11, plat12, plat13, plat14, plat15, plat16, plat17, plat18
-            }[sType.intValue];
+        if (sType.intValue >= 0 && sType.intValue < platSprites.Length)
+            sprite.sprite = platSprites[sType.intValue];
     }
 }
