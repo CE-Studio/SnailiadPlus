03c579f7539e0ae1f93f6901e4547603b052b485
clarence112 <clarencepacelli@gmail.com>
2025-03-16 06:20:48 -0400

    bock stuff and fix visual jitter

--- addons/cutprog/blocks/outsocket.gd
+++ addons/cutprog/blocks/outsocket.gd
@@ -3,4 +3,25 @@ class_name Outsocket
 extends TextureRect
 
 
-var type:int = 0
+const TEXTURES = [
+	preload("uid://dhuss2ltx0bac"), #bool
+	preload("uid://cad6u5u1gf3hw"), #str
+	preload("uid://wipjg26ehwi5"), #ACTOR,
+	preload("uid://d2nab8bc05grq"), #INT,
+	#FLOAT,
+	#TEXTURE2D,
+	#VECTOR2,
+	#NODE2D,
+]
+
+
+var type:int = 0:
+	set(value):
+		type = value
+		var texid = clampi(type, 0, TEXTURES.size() - 1)
+		texture = TEXTURES[texid]
+
+
+func _init() -> void:
+	var texid = clampi(type, 0, TEXTURES.size() - 1)
+	texture = TEXTURES[texid]
