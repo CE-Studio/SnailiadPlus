e05759d906ea93880888a2f936f0358558867fe1
clarence112 <clarencepacelli@gmail.com>
2025-03-16 00:38:43 -0400

    camera border

---
+++ Scripts/Classes/CameraBorder.gd
@@ -0,0 +1,13 @@
+@icon("uid://el86xs2vjg0b")
+class_name CameraBorder
+extends Path2D
+
+
+func _ready() -> void:
+	assert(global_position == Vector2.ZERO, "Camera border node MUST be centered")
+
+
+func get_closest_point_to(pos:Vector2) -> Vector2:
+	if Geometry2D.is_point_in_polygon(pos, curve.get_baked_points()):
+		return Vector2(pos)
+	return curve.get_closest_point(pos)
