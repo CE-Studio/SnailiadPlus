52d8fc9d8069daa45d103dea868e99f087f71611
clarence112 <clarencepacelli@gmail.com>
2025-04-05 19:08:20 -0400

    metadata, start work on layerized sprites

--- Scripts/Classes/jsonSprite2D.gd
+++ Scripts/Classes/jsonSprite2D.gd
@@ -5,8 +5,11 @@ extends Sprite2D
 
 const _DATA_MATCH = {
 	"tiles": [0, 0],
+	"layerize": false,
+	"meta": {},
 	"animations": {
 		"an_action": {
+			"colors": [0],
 			"fps": 30,
 			"loop": true,
 			"loop_point": 0, #optional
@@ -21,6 +24,7 @@ const _DATA_MATCH = {
 }
 
 
+@export var normal_npc := false 
 @export_file("*.json") var texture_path:String
 var data:Dictionary
 var is_ready := false
@@ -30,12 +34,14 @@ var action:String:
 		_index = 0
 		_timer = -2
 		_check_action()
+var meta:Dictionary = {}
 
 
 var _timer := 0.0
 var _has_action := false
 var _recheck := false
 var _index:int = 0
+var _children:Array[Sprite2D] = []
 
 
 static var _imgcache := {}
@@ -60,8 +66,10 @@ func _ready() -> void:
 		assert(false, "Path is not a json file!")
 		return
 
-
-	if _imgcache.has(pathtrimmed):
+	
+	if normal_npc:
+		texture = preload("uid://cp668vus8s3n0")
+	elif _imgcache.has(pathtrimmed):
 		texture = _imgcache[pathtrimmed]
 	else:
 		var found := false
@@ -152,6 +160,23 @@ func _ready() -> void:
 	
 	hframes = data["tiles"][0]
 	vframes = data["tiles"][1]
+	meta = data["meta"]
+	
+	if data["layerize"]:
+		var layers:Dictionary[int, Image]
+		var img := texture.get_image()
+		var sz = img.get_size()
+		for x in sz.x:
+			for y in sz.y:
+				var col := img.get_pixel(x, y)
+				if col.a8 != 255:
+					var c32 := col.to_rgba32()
+					if not (c32 in layers):
+						layers[c32] = Image.create_empty(sz.x, sz.y, false, Image.FORMAT_RGBA8)
+						layers[c32].fill(Color.TRANSPARENT)
+					var l := layers[c32]
+					l.set_pixel(x, y, Color.WHITE)
+		print(layers)
 	is_ready = true
 
 
