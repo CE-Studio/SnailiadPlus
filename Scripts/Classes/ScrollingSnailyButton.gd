fd96ea1dbc9e9dab21cd4b04f906759c5f0c2e1f
EpsilonTheDerg <jamesclamey@gmail.com>
2025-05-02 01:19:02 -0400

    Menu progress, ScrollingSnailyButton foundation

--- Scripts/Classes/ScrollingSnailyButton.gd
+++ Scripts/Classes/ScrollingSnailyButton.gd
@@ -1,21 +1,22 @@
 @tool
 @icon("res://Editor/ico/SnailyButton.svg")
-class_name SnailyButton
-extends PanelContainer
+class_name ScrollingSnailyButton
+extends Control
 
 #region Variables
 const COLOR_ENABLED = Color8(252, 252, 252)
 const COLOR_DISABLED = Color8(200, 192, 192)
 
-@export var text_id:String = ""
-@export var quick_load_layer:String = ""
-@export var back_one_layer:bool = false
+@export var header_id:String = ""
+@export var cycle_options:Array[String] = []
+@export var focus_option:int = 0
+@export var loop:bool = true
 @export var grab_focus_on_load:bool = false
 @export var disabled = false
-@export var hide_frame = false:
+@export var minimum_x:int:
 	set(value):
-		hide_frame = value
-		self_modulate = Color(1.0, 1.0, 1.0, 0.0 if hide_frame else 1.0)
+		minimum_x = value
+		custom_minimum_size.x = minimum_x
 
 var focused:bool = false
 var mouse_over:bool = false
@@ -27,47 +28,63 @@ var can_focus:bool = true:
 			has_played_focus_sound = false
 		focus_mode = Control.FOCUS_ALL if value else Control.FOCUS_NONE
 var has_played_focus_sound:bool = false
+var selected_option:int
 
-signal button_pressed(value)
+var selected:bool = false
+var left_neighbor:NodePath
+var right_neighbor:NodePath
 
-@onready var text:SnailyText
+signal option_cycled(value)
+
+@onready var header:SnailyText
+@onready var option:SnailyText
+@onready var tex_left:TextureRect
+@onready var tex_right:TextureRect
 @onready var sfx_focus:AudioStreamPlayer
 @onready var sfx_select:AudioStreamPlayer
 #endregion
 
-
 func _ready() -> void:
-	text = $"MarginContainer/SnailyText"
+	header = $"Main/TopText/SnailyText"
+	option = $"Main/Scroller/Frame/HBoxContainer/MarginContainer/SnailyText"
+	tex_left = $"Main/Scroller/Left"
+	tex_right = $"Main/Scroller/Right"
 	sfx_focus = $"AudioGroup/Focus"
 	sfx_select = $"AudioGroup/Select"
 	
-	text.set_alignment(HORIZONTAL_ALIGNMENT_CENTER, VERTICAL_ALIGNMENT_TOP)
-	text.add_shadow(1)
+	header.set_alignment(HORIZONTAL_ALIGNMENT_CENTER, VERTICAL_ALIGNMENT_TOP)
+	header.add_shadow(1)
+	option.add_shadow(1)
 	if not Engine.is_editor_hint():
 		origin = position
-		if text_id.strip_edges() == "":
-			text.set_snaily_text("Text!!")
+		if header_id.strip_edges() == "":
+			header.set_snaily_text("Text!!")
 		else:
-			text.set_snaily_text(Statics.get_text(text_id))
+			header.set_snaily_text(Statics.get_text(header_id))
+		if cycle_options.size() == 0:
+			option.set_snaily_text(Statics.get_text("menu_option_scroller_none"))
+			selected_option = -1
+			disabled = true
+		else:
+			while focus_option < 0:
+				focus_option += cycle_options.size()
+			selected_option = focus_option % cycle_options.size()
+			option.set_snaily_text(Statics.get_text(cycle_options[selected_option]))
 		if can_focus and grab_focus_on_load and not disabled:
 			grab_focus()
-		text.modulate = COLOR_DISABLED if disabled else COLOR_ENABLED
-		hide_frame = hide_frame
+		header.modulate = COLOR_DISABLED if disabled else COLOR_ENABLED
+		option.modulate = COLOR_DISABLED if disabled else COLOR_ENABLED
+		tex_left.modulate = COLOR_DISABLED if disabled else COLOR_ENABLED
+		tex_right.modulate = COLOR_DISABLED if disabled else COLOR_ENABLED
 
 
 func _process(delta: float) -> void:
-	if focused and not disabled:
-		if (mouse_over and (Input.is_action_just_pressed("UIClick"))
-		or Input.is_action_just_pressed("Jump")):
-			if quick_load_layer.strip_edges() != "":
-				button_pressed.emit(quick_load_layer)
-			else:
-				button_pressed.emit()
 	pass
 
 
-func set_text(_text:String) -> void:
-	text.set_snaily_text(_text)
+
+func set_header(_text:String) -> void:
+	header.set_snaily_text(_text)
 
 
 func free_safely() -> void:
