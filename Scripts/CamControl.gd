866bfd6f9d2a7f13a17086b15d59cc4dc0ce2d9d
EpsilonTheDerg <jamesclamey@gmail.com>
2025-09-24 21:44:18 +0100

    Progress updating camera movement

--- Scripts/CamControl.gd
+++ Scripts/CamControl.gd
@@ -17,6 +17,15 @@ var player:Player
 var ease_rate:float = 4.0
 var offset:Vector2 = Vector2(200, 120)
 var border:CameraBorder = null
+
+#region New follow vars
+const NF_OFFSET_MAX:Vector2 = Vector2(48.0, 32.0)
+const NF_OFFSET_ADJUST_DELAY:float = 0.75
+const NF_OFFSET_EASE_RATE:float = 32.0
+const NF_OFFSET_MAX_FALL_MULT:Vector2 = Vector2(0.5, 2.0)
+var nf_offset:Vector2 = Vector2.ZERO
+var nf_delay_timer:float = 0.0
+#endregion
 #endregion
 
 
@@ -31,7 +40,7 @@ func _process(delta):
 			if player != null:
 				pos = pos.lerp(player.position - offset, ease_rate * delta)
 		CamStates.FOLLOW_NEW:
-			pass
+			pos = _tick_new_follow(pos, delta)
 		CamStates.TARGET_POINT:
 			pass
 		CamStates.TARGET_ENTITY:
@@ -55,6 +64,60 @@ func _process(delta):
 						pos.y = clampf(pos.y, -INF, child.position.y + buffer)
 		#endregion
 	UICore.instance.position = pos
+
+
+func _tick_new_follow(pos:Vector2, delta:float) -> Vector2:
+	var move_vector:Vector2 = SInput.vector_move()
+	if move_vector != Vector2.ZERO:
+		nf_delay_timer += delta
+	else:
+		nf_delay_timer = 0.0
+	
+	var walled:bool = (
+		player.gravity_dir == Statics.DirsSurface.LWALL or
+		player.gravity_dir == Statics.DirsSurface.RWALL
+	)
+	if player.grounded:
+		if nf_delay_timer >= NF_OFFSET_ADJUST_DELAY:
+			if walled:
+				nf_offset.y = clampf(
+					nf_offset.y + (NF_OFFSET_EASE_RATE * delta * move_vector.y),
+					-NF_OFFSET_MAX.y, NF_OFFSET_MAX.y
+				)
+			else:
+				nf_offset.x = clampf(
+					nf_offset.x + (NF_OFFSET_EASE_RATE * delta * move_vector.x),
+					-NF_OFFSET_MAX.x, NF_OFFSET_MAX.x
+				)
+	else:
+		match player.gravity_dir:
+			Statics.DirsSurface.FLOOR:
+				nf_offset.y = clampf(
+					nf_offset.y + (NF_OFFSET_EASE_RATE * delta),
+					-INF, NF_OFFSET_MAX.y * NF_OFFSET_MAX_FALL_MULT.y
+				)
+				nf_offset.x = NF_OFFSET_MAX.x * NF_OFFSET_MAX_FALL_MULT.x * move_vector.x
+			Statics.DirsSurface.LWALL:
+				nf_offset.x = clampf(
+					nf_offset.x - (NF_OFFSET_EASE_RATE * delta),
+					-INF, NF_OFFSET_MAX.x * NF_OFFSET_MAX_FALL_MULT.x
+				)
+				nf_offset.y = NF_OFFSET_MAX.y * NF_OFFSET_MAX_FALL_MULT.y * move_vector.y
+			Statics.DirsSurface.RWALL:
+				nf_offset.x = clampf(
+					nf_offset.x + (NF_OFFSET_EASE_RATE * delta),
+					-INF, NF_OFFSET_MAX.x * NF_OFFSET_MAX_FALL_MULT.x
+				)
+				nf_offset.y = NF_OFFSET_MAX.y * NF_OFFSET_MAX_FALL_MULT.y * move_vector.y
+			Statics.DirsSurface.CEILING:
+				nf_offset.y = clampf(
+					nf_offset.y - (NF_OFFSET_EASE_RATE * delta),
+					-INF, NF_OFFSET_MAX.y * NF_OFFSET_MAX_FALL_MULT.y
+				)
+				nf_offset.x = NF_OFFSET_MAX.x * NF_OFFSET_MAX_FALL_MULT.x * move_vector.x
+	
+	pos = pos.lerp(player.position - offset + nf_offset, ease_rate * delta)
+	return pos
 
 
 func set_layer_position(new_pos:Vector2):
