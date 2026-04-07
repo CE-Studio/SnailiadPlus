a6a0385da8bca2184661f918251ce85ac8587123
EpsilonTheDerg <jamesclamey@gmail.com>
2026-04-07 01:50:14 +0100

    Giga environment progress (god I am slow)

--- Scripts/WorldElements/Backgrounds/GigaGround.gd
+++ Scripts/WorldElements/Backgrounds/GigaGround.gd
@@ -2,21 +2,20 @@ extends Node2D
 
 
 #region Variables
-const SPR_SIZE:int = 48
+const SPR_SIZE:int = 16
+const FADE_ALPHA:float = 0.1
+const FADE_MIN:float = 16.0
+const FADE_DISTANCE:float = 128.0
 
 var elapsed:float = 0.0
 var effect_dir:Vector2 = Vector2.DOWN
-var anim_main:String = "floor_main"
-var anim_eff:String = "floor_effect"
+var anim_prefix:String = "floor_"
 var anim_path:String = ""
 
 @export var segments:int = 1
 @export var surface:Statics.DirsSurface
 
-@onready var effect_group_0:Node2D = $"EffectGroup0"
-@onready var effect_group_1:Node2D = $"EffectGroup1"
-@onready var sprites_main:Array[JsonSprite2D] = [ $"Main" ]
-@onready var sprites_effect:Array[JsonSprite2D] = [ $"EffectGroup0/Effect", $"EffectGroup1/Effect" ]
+@onready var sprites:Array[JsonSprite2D] = [ $"Sprite" ]
 #endregion
 
 
@@ -24,48 +23,44 @@ func _ready() -> void:
 	if segments < 1:
 		segments = 1
 	
-	anim_path = sprites_main[0].texture_path
+	anim_path = sprites[0].texture_path
 	
 	var extend_dir:Vector2 = Vector2.RIGHT
 	match surface:
 		Statics.DirsSurface.LWALL:
 			effect_dir = Vector2.LEFT
 			extend_dir = Vector2.DOWN
-			anim_main = "lwall_main"
-			anim_eff = "lwall_effect"
+			anim_prefix = "lwall_"
 		Statics.DirsSurface.RWALL:
 			effect_dir = Vector2.RIGHT
 			extend_dir = Vector2.DOWN
-			anim_main = "rwall_main"
-			anim_eff = "rwall_effect"
+			anim_prefix = "rwall_"
 		Statics.DirsSurface.CEILING:
 			effect_dir = Vector2.UP
-			anim_main = "ceiling_main"
-			anim_eff = "ceiling_effect"
+			anim_prefix = "ceiling_"
 	
 	segments -= 1
-	sprites_main[0].position -= extend_dir * segments * (SPR_SIZE * 0.5)
-	sprites_effect[0].position -= extend_dir * segments * (SPR_SIZE * 0.5)
-	sprites_effect[1].position = sprites_effect[0].position
-	var origin:Vector2 = sprites_main[0].position
+	sprites[0].position -= extend_dir * segments * (SPR_SIZE * 0.5)
+	var origin:Vector2 = sprites[0].position
 	for i in range(segments):
 		var j:int = i + 1
 		var new_main:JsonSprite2D = JsonSprite2D.new()
 		new_main.texture_path = anim_path
 		add_child(new_main)
-		sprites_main.append(new_main)
+		sprites.append(new_main)
 		new_main.position = origin + extend_dir * j * SPR_SIZE
-		var new_eff0:JsonSprite2D = JsonSprite2D.new()
-		new_eff0.texture_path = anim_path
-		effect_group_0.add_child(new_eff0)
-		sprites_effect.append(new_eff0)
-		new_eff0.position = new_main.position
-		var new_eff1:JsonSprite2D = JsonSprite2D.new()
-		new_eff1.texture_path = anim_path
-		effect_group_1.add_child(new_eff1)
-		sprites_effect.append(new_eff1)
-		new_eff1.position = new_main.position
-	for spr in sprites_main:
-		spr.action = anim_main
-	for spr in sprites_effect:
-		spr.action = anim_eff
+	for spr in sprites:
+		spr.action = anim_prefix + "intro"
+		spr._process(0.0)
+
+
+func _process(_delta: float) -> void:
+	#pass
+	if Player.instance:
+		var p_pos:Vector2 = Player.instance.position
+		for spr in sprites:
+			spr.modulate.a = FADE_ALPHA
+			var dist:float = spr.global_position.distance_to(p_pos)
+			if dist < FADE_DISTANCE:
+				var weight:float = inverse_lerp(FADE_MIN, FADE_DISTANCE, dist)
+				spr.modulate.a = lerpf(1.0, FADE_ALPHA, clampf(weight, 0.0, 1.0))
