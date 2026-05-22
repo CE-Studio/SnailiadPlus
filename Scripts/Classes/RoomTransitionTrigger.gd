b10efa4556a4257fbeac5c341ea2daadb55f3d48
EpsilonTheDerg <jamesclamey@gmail.com>
2026-05-22 12:06:58 +0100

    tweak: snelks despawn at room transitions

--- Scripts/Classes/RoomTransitionTrigger.gd
+++ Scripts/Classes/RoomTransitionTrigger.gd
@@ -9,7 +9,7 @@ extends Node2D
 @export var exit_transition:int
 
 ## The collidable area component of this trigger
-@onready var area:Area2D = $"Area2D"
+@onready var area:Area2D = $"PlayerArea"
 ## The marker the player will spawn relative to if exiting a transition through this trigger
 @onready var exit_marker:Marker2D = $"Marker2D"
 
@@ -32,14 +32,22 @@ func _process(_delta: float) -> void:
 		spawn_buffer_frames -= 1
 
 
-## Called when the player enters this trigger's area
-func _on_player_enter(_area):
+## Called when the player or any enemy enters this trigger's area
+func _on_player_enter(_body):
 	if exit_room != "" and not_exiting and spawn_buffer_frames == 0:
 		not_exiting = false
 		var offset = GameCore.instance.player.body.global_position - area.global_position
 		UICore.instance.color_cover.call_thread_safe("set_color", Color8(0, 0, 0, 120))
 		GameCore.instance.call_deferred("spawn_room", exit_room, exit_transition, offset)
 		UICore.instance.color_cover.set_new_fade(Color8(0, 0, 0, 255), Color8(0, 0, 0, 0), 0.25)
+
+
+## Called when any non-player entity enters this trigger's area
+func _on_body_enter(body) -> void:
+	if body is Enemy:
+		match body.my_type:
+			Enemy.EnemyTypes.SNELK:
+				body.queue_free()
 
 
 ## Called when this trigger's parent room is despawned
