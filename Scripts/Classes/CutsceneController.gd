938716b32e4387a3bf95621cb2b7f5ecc2db3084
EpsilonTheDerg <jamesclamey@gmail.com>
2026-05-24 17:46:10 +0100

    tweak: uiAccept input advances text

--- Scripts/Classes/CutsceneController.gd
+++ Scripts/Classes/CutsceneController.gd
@@ -347,3 +347,5 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"skipTalking"):
 		if texlabel.is_typing:
 			texlabel.skip_typing()
+		else:
+			StaticProcess.cut_advance.emit()
