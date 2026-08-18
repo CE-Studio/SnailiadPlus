cb3408154b4d34d478440cbb24daf9e7428e83e5
EpsilonTheDerg <jamesclamey@gmail.com>
2026-08-18 13:26:02 +0100

    tweak: enemy bullet refinements, damage balancing

--- Scripts/Bullets/Enemy/EnemyBoomerangBlue.gd
+++ Scripts/Bullets/Enemy/EnemyBoomerangBlue.gd
@@ -9,19 +9,8 @@ func _spawn(dir:Vector2, speed:float, play_sound:bool = true) -> void:
 	super._spawn(dir, speed, play_sound)
 	velocity = speed
 	velocity_init = speed
-	#region Direction animation
-	var anim_name = ""
-	if dir.y < -0.3827:
-		anim_name += "U"
-	elif dir.y > 0.3827:
-		anim_name += "D"
-	if dir.x < -0.3827:
-		anim_name += "L"
-	elif dir.x > 0.3827:
-		anim_name += "R"
-	sprite.action = anim_name
-	sprite._process(0.0)
-	#endregion
+	sprite.visible = true
+	_infer_direction_anim()
 
 
 func _physics_process(delta: float) -> void:
