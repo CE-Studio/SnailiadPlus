787342d7444bf85fd8efb93b64c79d552f7743e5
EpsilonTheDerg <jamesclamey@gmail.com>
2026-08-19 01:03:28 +0100

    chore: sprite refactor - batty bat, epic banner

--- Scripts/Enemies/BattyBat.gd
+++ Scripts/Enemies/BattyBat.gd
@@ -14,23 +14,17 @@ var move_timeout:float = MOVE_TIMEOUT
 var is_flying:bool = false
 var facing_left:bool = false
 
-@onready var sfx_fly:AudioStreamPlayer = $"Fly"
+@export var sfx_fly:AudioStreamPlayer
 #endregion
 
 
 func _ready() -> void:
 	my_type = EnemyTypes.BATTYBAT
-	hitbox = $"Area2D"
-	sprite = $"JsonSprite2D"
-	vis = $"VisibleOnScreenNotifier2D"
 	super.spawn()
 	
 	if display_mode:
 		facing_left = randf() < 0.5
-		sprite.action = "fly_" + ("left" if facing_left else "right")
-	else:
-		facing_left = _get_left()
-		sprite.action = "idle_" + ("left" if facing_left else "right")
+		sprite.play("fly_" + ("left" if facing_left else "right"))
 
 
 func _physics_process(delta: float) -> void:
@@ -40,22 +34,17 @@ func _physics_process(delta: float) -> void:
 	
 	move_timeout -= delta
 	if (vis.is_on_screen() and move_timeout <= 0.0 and not is_flying
-	and abs(GameCore.instance.player.position.x - position.x) < REACT_DISTANCE):
+	and abs(Player.instance.position.x - position.x) < REACT_DISTANCE):
 		facing_left = _get_left()
 		velocity.x = SPEED * (-1 if facing_left else 1)
-		velocity.y = sqrt(abs(GameCore.instance.player.position.y - position.y + 40)) * SPEED * 0.25
-		sprite.action = "fly_" + ("left" if facing_left else "right")
+		velocity.y = sqrt(abs(Player.instance.position.y - position.y + 40)) * SPEED * 0.25
+		sprite.play("fly_" + ("left" if facing_left else "right"))
 		is_flying = true
 		sfx_fly.play()
-	elif not is_flying:
-		var new_left = _get_left()
-		if new_left != facing_left:
-			facing_left = new_left
-			sprite.action = "idle_" + ("left" if facing_left else "right")
-	else:
+	elif is_flying:
 		velocity.y -= SPEED * 2 * delta
 	position += velocity * delta
 
 
 func _get_left() -> bool:
-	return GameCore.instance.player.position.x < position.x
+	return Player.instance.position.x < position.x
