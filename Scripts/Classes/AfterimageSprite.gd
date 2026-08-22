dfaa58a41f60a4f4c97d2b5ef71fb1c423053b1e
EpsilonTheDerg <jamesclamey@gmail.com>
2026-08-22 20:12:27 +0100

    chore: update AfterimageSprite2D to new architecture

--- Scripts/Classes/AfterimageSprite.gd
+++ Scripts/Classes/AfterimageSprite.gd
@@ -1,6 +1,6 @@
 # Copyright 2026 CE-Studio: AGPL-3.0-only
-class_name AfterimageSprite
-extends Sprite2D
+class_name AfterimageSprite2D
+extends SnailySprite2D
 
 
 ## The alpha (from 0.0 to 1.0) that this sprite starts at
@@ -16,23 +16,23 @@ var elapsed:float = 0.0
 
 ## Properly initializes this sprite with whatever alpha, fade time, and other configurations
 ## are required
-func setup(_source_sprite:JsonSprite2D, _frame_coords:Vector2i, _start_a:float, _end_a:float, _fade_time:float, _z:int) -> void:
-	texture = _source_sprite.texture
-	hframes = _source_sprite.hframes
-	vframes = _source_sprite.vframes
-	frame_coords = _frame_coords
+func setup(_source:SnailySprite2D, _anim:String, _start_a:float, _end_a:float, _fade_time:float, _z:int) -> void:
+	sprite_frames = _source.sprite_frames
+	flip_h = _source.flip_h
+	flip_v = _source.flip_v
+	play(_anim)
+	pause()
 	start_a = _start_a
 	end_a = _end_a
 	fade_time = _fade_time
 	modulate.a = _start_a
 	z_index = _z
-	global_position = _source_sprite.global_position
+	global_position = _source.global_position
 
 
 func _process(delta: float) -> void:
-	if texture:
-		var weight:float = inverse_lerp(0.0, fade_time, elapsed)
-		modulate.a = lerp(start_a, end_a, weight)
-		if elapsed > fade_time:
-			queue_free()
-		elapsed += delta
+	var weight:float = inverse_lerp(0.0, fade_time, elapsed)
+	modulate.a = lerp(start_a, end_a, weight)
+	if elapsed > fade_time:
+		queue_free()
+	elapsed += delta
