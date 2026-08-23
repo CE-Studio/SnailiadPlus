e4efe5639090e1f761f6a48dc3a7bffd56d0a4d0
EpsilonTheDerg <jamesclamey@gmail.com>
2026-08-23 22:41:04 +0100

    chore: sprite refactor - intro

--- Scripts/UI/IntroCinematic.gd
+++ Scripts/UI/IntroCinematic.gd
@@ -7,7 +7,6 @@ extends Node2D
 #region Variables
 const PATTERN_GRID:Vector2i = Vector2i(12, 8)
 const PATTERN_SIZE:Vector2i = Vector2i(48, 48)
-const PATTERN_PATH:String = "res://Assets/Images/Intro/IntroBGPatterns.json"
 const PATTERN_DIR:Vector2i = Vector2i(-34, -34)
 const CHAR_DELAY:float = 0.067
 const SKIP_THRESHOLD:float = 1.0
@@ -22,22 +21,28 @@ var story_strings:PackedStringArray = [
 	tr(&" You can do it, %s!!        \n        It's up to you!!  Good luck!!")
 ]
 
+## The [SpriteFrames] used for the scrolling background patterns
+var pattern_frames:SpriteFrames = load("uid://y07yao22iple")
 ## The amount of time in seconds that this node has been active
 var elapsed:float = 0.0
 ## The amount of time in seconds since the fadeout was started
 var fade_elapsed:float = 0.0
 ## List of all active background pattern sprites
-var bg_patterns:Array[JsonSprite2D] = []
+var bg_patterns:Array[SnailySprite2D] = []
 ## The string to print on screen
 var target_str:String = ""
 ## Pointer used to determine which character to add to the printed string next
 var char_i:int = 0
 ## The time in seconds left until the next character can be printed
 var char_timeout:float = 0.0
+## Will be set if text is currently being printed
+var is_printing:bool = false
 ## The character currently being played as
 var player_char:Player.Players = Player.Players.SNAILY
 ## Will be set if the fadeout has started
 var started_fade:bool = false
+## Will be set if playback is currently paused and the intro is awaiting input to advance
+var is_awaiting_input:bool = false
 ## Reference to the menu scene
 var menu:MainMenu
 
@@ -46,48 +51,39 @@ var menu:MainMenu
 ## The parent node that holds all background pattern sprites as children
 @export var pattern_group:Node2D
 ## The parent node for the fourth story page, which shows the player character
-@export var story4_parent:Node2D
+@export var story4:SnailySprite2D
 ## A multiplier applied to the background pattern scroll speed
 @export var pattern_speed_mult:float = 1.0
 ## The sound that plays when a new character is added to the visible string
 @export var sfx_text:AudioStreamPlayer
 ## The sound that plays when the story art is changed to a new frame
 @export var sfx_page:AudioStreamPlayer
+## The sound that plays when an advance input is received
+@export var sfx_advance:AudioStreamPlayer
 ## The color cover that fades the intro out
 @export var cover:ColorCover
+## The [AnimationPlayer] that drives the intro
+@export var anim:AnimationPlayer
+## The sprite that appears to prompt manual input to advance the intro
+@export var advance_prompt:SnailySprite2D
 #endregion
 
 
 func _ready() -> void:
 	_create_bg()
 	clear_text()
+	advance_prompt.visible = false
 
 
 func setup_player(player:Player.Players) -> void:
-	player_char = player
-	var story4_anim:String = "res://Assets/Images/Intro/Intro4%s.json"
 	match player:
-		Player.Players.SNAILY:
-			story4_anim = story4_anim % "A"
-			story_strings[4] = story_strings[4] % GlobalText.characters[0][1]
-		Player.Players.SLUGGY:
-			story4_anim = story4_anim % "B"
-			story_strings[4] = story_strings[4] % GlobalText.characters[1][1]
-		Player.Players.UPSIDE:
-			story4_anim = story4_anim % "C"
-			story_strings[4] = story_strings[4] % GlobalText.characters[2][1]
-		Player.Players.LEGGY:
-			story4_anim = story4_anim % "D"
-			story_strings[4] = story_strings[4] % GlobalText.characters[3][1]
-		Player.Players.BLOBBY:
-			story4_anim = story4_anim % "E"
-			story_strings[4] = story_strings[4] % GlobalText.characters[4][1]
-		Player.Players.LEECHY:
-			story4_anim = story4_anim % "F"
-			story_strings[4] = story_strings[4] % GlobalText.characters[5][1]
-	var story4_spr:JsonSprite2D = JsonSprite2D.new()
-	story4_spr.texture_path = story4_anim
-	story4_parent.add_child(story4_spr)
+		Player.Players.SNAILY: story4.play("a")
+		Player.Players.SLUGGY: story4.play("b")
+		Player.Players.UPSIDE: story4.play("c")
+		Player.Players.LEGGY: story4.play("d")
+		Player.Players.BLOBBY: story4.play("e")
+		Player.Players.LEECHY: story4.play("f")
+	story_strings[4] = story_strings[4] % GlobalText.characters[player as int][1]
 
 
 func _process(delta: float) -> void:
@@ -118,9 +114,10 @@ func _process(delta: float) -> void:
 				advance_from_newline and (this_char != " " and this_char != "\n")):
 					printed_next = true
 				char_i += 1
-				play_sound = this_char != " "
+				play_sound = this_char != " " and this_char != "\n"
 			if play_sound:
 				sfx_text.play()
+	is_printing = char_i < target_str.length()
 	
 	if elapsed >= SKIP_THRESHOLD and not started_fade and SInput.check_input(SInput.Inputs.PAUSE, true):
 		start_fade()
@@ -132,6 +129,11 @@ func _process(delta: float) -> void:
 			menu.music.volume_linear = clampf(menu.music.volume_linear - delta, 0.0, 1.0)
 		if fade_elapsed >= SCENE_THRESHOLD:
 			get_tree().change_scene_to_file("uid://ltxtlsrku2k0")
+	
+	if is_awaiting_input and not is_printing:
+		advance_prompt.visible = true
+		if SInput.check_input(SInput.Inputs.UI_ACCEPT, true):
+			continue_anim()
 
 
 ## Automatically creates all background patterns
@@ -140,16 +142,15 @@ func _create_bg() -> void:
 	for y in range(PATTERN_GRID.y):
 		variant = y % 2 == 1
 		for x in range(PATTERN_GRID.x):
-			var new_spr:JsonSprite2D = JsonSprite2D.new()
-			new_spr.texture_path = PATTERN_PATH
-			new_spr.load_autoplay = [
-				("2" if variant else "1") + "a",
-				("2" if variant else "1") + "b",
-				("2" if variant else "1") + "c",
-				("2" if variant else "1") + "d",
-			]
+			var new_spr:SnailySprite2D = SnailySprite2D.new()
+			new_spr.sprite_frames = pattern_frames
 			pattern_group.add_child(new_spr)
 			new_spr.position = Vector2i(x, y) * PATTERN_SIZE
+			match randi_range(0, 3):
+				0: new_spr.play(("2" if variant else "1") + "a")
+				1: new_spr.play(("2" if variant else "1") + "b")
+				2: new_spr.play(("2" if variant else "1") + "c")
+				3: new_spr.play(("2" if variant else "1") + "d")
 			bg_patterns.append(new_spr)
 			variant = not variant
 
@@ -173,3 +174,16 @@ func start_fade() -> void:
 	var end_color:Color = cover.start_color
 	end_color.a = 1.0
 	cover.set_new_fade(cover.start_color, end_color, 1.0)
+
+
+## Pauses animation playback to wait for player input
+func await_input() -> void:
+	is_awaiting_input = true
+	anim.pause()
+
+## Resumes animation playback
+func continue_anim() -> void:
+	is_awaiting_input = false
+	advance_prompt.visible = false
+	anim.play()
+	sfx_advance.play()
